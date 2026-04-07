5933c552f84d84018dcef8b446d45c1947f680bb
Jordan Alexander Suwito <jordansuwito@gmail.com>
2026-04-07 22:23:37 +0700

    fix AppRoutes commit

--- frontend/src/config/AppRoutes.tsx
+++ frontend/src/config/AppRoutes.tsx
@@ -1,16 +1,19 @@
 import { lazy } from "react";
 import { Route, Routes } from "react-router";
 import { MenuLayout } from "../components/MenuLayout";
-import { TemplateLayout } from "../components/TemplateLayout";
-import OrderType from "../pages/Order/OrderType/OrderType";
-import CustomizeOrder from "../pages/Order/CustomizeOrder/CustomizeOrder";
-import OrderCart from "../pages/Order/OrderCart/OrderCart";
+
 import OrderLayout from "../components/OrderLayout";
 import SetQuantityPage from "../pages/Order/SetQuantityPage/SetQuantityPage";
 
 const MenuPage = lazy(() => import("../pages/Menu/MenuPage"));
+
 const PaymentPage = lazy(() => import("../pages/Payment/PaymentPage"));
 const PaymentSuccess = lazy(() => import("../pages/Payment/PaymentSuccess"));
+
+const OrderTypePage = lazy(() => import("../pages/Order/OrderType/OrderType"));
+const CustomizeOrderPage = lazy(() => import("../pages/Order/CustomizeOrder/CustomizeOrder"));
+const OrderCartPage = lazy(() => import("../pages/Order/OrderCart/OrderCart"));
+const RecomendationPage = lazy(() => import("../pages/Order/Recomendation/RecomendationPage"));
 
 const PackageSelection = lazy(() => import("../pages/Order/PackageSelection/PackageSelectionBurger"));
 
@@ -26,9 +29,14 @@ export const AppRoutes = () => {
       
       <Route path="/order/package-selection" element={<PackageSelection />} />
 
-      {/* <Route element={<TemplateLayout />}> */} // Ubah nama Layout sesuai
-      fitur kalian // Masukkin Web Kalian Di sini
-      {/* </Route> */}
+      <Route element={<OrderLayout />}>
+                <Route path="/order" element={<OrderTypePage />} />
+                <Route path="/customizeOrder/:id" element={<CustomizeOrderPage />} />
+                <Route path="/cart" element={<OrderCartPage />} />
+                <Route path="/setQuantity/:id" element={<SetQuantityPage/>} />
+                <Route path="/recomendation" element={<RecomendationPage/>} />
+      </Route>
+
     </Routes>
   );
 };
