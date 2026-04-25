1e7221a961ce737b6fc26e337fa3b55316779d8e
nathaliewjy <nathalie.tantiwijaya@gmail.com>
2026-04-25 15:10:07 +0700

    push after merge

--- backend/src/routes/kategoriRoutes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { KategoriController } from "../controllers/KategoriController";
-
-const router: Router = Router();
-
-router.get("/", KategoriController.getAll);
-router.get("/:id", KategoriController.getById)
-
-export default router;
