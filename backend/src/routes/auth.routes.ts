99fb62fa32ae2c43c63ac3ffd90cac379af360cc
Jordan Alexander Suwito <jordansuwito@gmail.com>
2026-05-02 12:25:38 +0700

    fix auth routes

--- backend/src/routes/auth.routes.ts
+++ backend/src/routes/auth.routes.ts
@@ -4,11 +4,9 @@ import auth from "../middlewares/auth.middleware";
 
 const router: Router = Router();
 
-// router.post("/register", AuthController.register);
 router.post("/login", AuthController.login);
 router.post("/logout", auth, AuthController.logout);
 router.get("/me", auth, AuthController.me);
-router.post("/logout", auth, AuthController.logout);
 router.post("/forgot-password", AuthController.forgotPassword);
 router.post("/reset-password", AuthController.resetPassword);
 
